2bea72b72dfeb40fd5a6b38540cebbce272d6b8e
FrancisLaTruelle <francislatruelle@gmail.com>
2021-02-07 23:21:18 +0100

    1.1.0 update

--- README.md
+++ README.md
@@ -14,9 +14,8 @@ Be careful to respect these things for the module to work correctly:
   - Go to the configuration of your bot on your control panel of your Discord applications. You must activate the "Privileged Gateway Intents": activate "Presence Intent" and "Server Members Intent"
   - Don't forget to put the partial configuration in your client's definition in your code
 
-
 # Example
-Example of a basic bot handling reactions and messages using this module.
+Full example using disable All to disable (as on older versions of the module): 
 ```js
 
 const Discord = require("discord.js")
@@ -28,7 +27,8 @@ const disableEmojis = require("discord-disable-emojis")
 
 client.on('ready', async () => {
     disableEmojis(client, {
-    // WARNING: For the you must use 🙂 and not :slight_smile: for example
+        disableAll: disable, // All emojis are allowed, you must forbid them (default: disable)
+        // WARNING: For the you must use 🙂 and not :slight_smile: for example
         disabledEmojisMessage: [ // Add as many emoji you want banned in messages
             "💩",
             "🖕"
@@ -49,3 +49,35 @@ client.on('messageReactionAdd', (messageReaction, user) => client.emit('checkEmo
 
 client.login("YOUR_SUPER_SECRET_TOKEN")
 ```
+
+Small part of an example to show how disableAll to enable: 
+
+```js
+client.on('ready', async () => {
+    disableEmojis(client, {
+        disableAll: enable, // All emojis are disabled, some must be authorized (default: disable)
+        // WARNING: For the you must use 🙂 and not :slight_smile: for example
+        allowedEmojisMessage: [ // Add as many emoji you want allowed in messages
+            "❤️",
+            "🍪"
+        ],
+        allowedEmojisReaction: [], // Add as many emoji as you want allowed in reactions (nothing: forbid everything, in this case the reactions are disabled)
+        DMwarnEmojisMessage: ":shield: **|** You are not allowed to use this emoji in your message !", // The message that the bot will send in DM to the user who uses a forbidden emoji in a message. Delete this line or enter "None" for any message sent
+        DMwarnEmojisReaction: ":shield: **|** You are not allowed to use this emoji in reaction !" // The message that the bot will send in DM to the user who reacts with a forbidden emoji. Remove this line or enter "None" for any message sent
+    })
+
+    // Rest of your code
+})
+```
+
+# Patch note
+Note: The DisableAll is by default disabled, this will not break old bots that update this module.
+__Versions :__
+- 1.1.0 : 
+    - Added DisableAll option (suggestion from Nokocchii)
+    - Added allowedEmojisMessage option (obvious)
+    - Added allowedEmojisReaction option (obvious again)
+    - Code optimization 
+    - Improvement of README
+- 1.0.0 :
+    - Creation of the module with its basic options 
--- discord-disable-emojis.js
+++ discord-disable-emojis.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js")
+const emojiRegex = require('emoji-regex/RGI_Emoji.js');
 
 module.exports = async (client, options) => {
 
-    const disabledEmojisMessage = (options && options.disabledEmojisMessage) || []
-    const disabledEmojisReaction = (options && options.disabledEmojisReaction) || []
+    const disableAll = (options && options.disableAll) || "disable"
 
     const DMwarnEmojisMessage = (options && options.DMwarnEmojisMessage) || "None"
     if (!DMwarnEmojisMessage) DMwarnEmojisMessage = "None"
@@ -11,28 +11,85 @@ module.exports = async (client, options) => {
     const DMwarnEmojisReaction = (options && options.DMwarnEmojisReaction) || "None"
     if (!DMwarnEmojisReaction) DMwarnEmojisReaction = "None"
 
-    client.on("checkEmojisMessage", async (message) => {
-        for (let i = 0; i < disabledEmojisMessage.length; i++) {
-            if (message.content.includes(disabledEmojisMessage[i])) {
-                message.delete().catch(e => {})
-                if (!(DMwarnEmojisMessage === "None")) return message.author.send(DMwarnEmojisMessage)
-            }
-        }
-    })
+    function DMwarnMessage(user) {
+        if (!(DMwarnEmojisMessage === "None")) return user.send(DMwarnEmojisMessage).catch(e => {})
+    }
+    function DMwarnReaction(user) {
+        if (!(DMwarnEmojisReaction === "None")) return user.send(DMwarnEmojisReaction).catch(e => {})
+    }
 
-    client.on("checkEmojisReaction", async (reaction, user) => {
-        if(reaction.message.partial) await reaction.message.fetch();
-        if(reaction.partial) await reaction.fetch();
+    if (disableAll === "disable") {
+        const disabledEmojisMessage = (options && options.disabledEmojisMessage) || []
+        const disabledEmojisReaction = (options && options.disabledEmojisReaction) || []
+
+        client.on("checkEmojisMessage", async (message) => {
+            for (let i = 0; i < disabledEmojisMessage.length; i++) {
+                if (message.content.includes(disabledEmojisMessage[i])) {
+                    message.delete().catch(e => {})
+                    DMwarnMessage(message.author)
+                }
+            }
+        })
     
-        let message = reaction.message;
-        if(!message) return;
-        if(user.bot) return;
-
-        for (let i = 0; i < disabledEmojisReaction.length; i++) {
-            if (reaction.emoji.name === disabledEmojisReaction[i]) {
-                reaction.users.remove(user.id)
-                if (!(DMwarnEmojisReaction === "None")) return message.author.send(DMwarnEmojisReaction)
+        client.on("checkEmojisReaction", async (reaction, user) => {
+            if(reaction.message.partial) await reaction.message.fetch();
+            if(reaction.partial) await reaction.fetch();
+        
+            let message = reaction.message;
+            if(!message) return;
+            if(user.bot) return;
+    
+            for (let i = 0; i < disabledEmojisReaction.length; i++) {
+                if (reaction.emoji.name === disabledEmojisReaction[i]) {
+                    reaction.users.remove(user.id)
+                    DMwarnReaction(user)
+                }
             }
-        }
-    })
+        })
+    } else if (disableAll === "enable") {
+        const allowedEmojisMessage = (options && options.allowedEmojisMessage) || ["💩", "🖕"]
+        const allowedEmojisReaction = (options && options.allowedEmojisReaction) || ["💩", "🖕"]
+
+        client.on("checkEmojisMessage", async (message) => {
+            if (!allowedEmojisMessage.length) {
+                let checked = message.content.match(emojiRegex())
+                if(checked && checked.length) {
+                    message.delete().catch(e => {})
+                    DMwarnMessage(message.author)
+                }
+            }
+
+            for (let i = 0; i < allowedEmojisMessage.length; i++) {
+                if (message.content.includes(allowedEmojisMessage[i])) {
+                    return
+                }
+            }
+            message.delete().catch(e => {})
+            DMwarnMessage(message.author)
+        })
+
+        client.on("checkEmojisReaction", async (reaction, user) => {
+            if(reaction.message.partial) await reaction.message.fetch();
+            if(reaction.partial) await reaction.fetch();
+        
+            let message = reaction.message;
+            if(!message) return;
+            if(user.bot) return;
+    
+            if (!allowedEmojisReaction.length) {
+                reaction.users.remove(user.id)
+                DMwarnReaction(user)
+            }
+
+            for (let i = 0; i < allowedEmojisReaction.length; i++) {
+                if (reaction.emoji.name === allowedEmojisReaction[i]) {
+                    return
+                }
+            }
+            reaction.users.remove(user.id)
+            DMwarnReaction(user)
+        })
+    } else {
+        throw new Error("The disableAll parameter is invalid (enable/disable)")
+    }
 }
